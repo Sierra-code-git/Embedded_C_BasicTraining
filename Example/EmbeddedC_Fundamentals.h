143956365f4eac09b0eb9239a31087ee700476c9
Sierra-code-git <Nireeksha@sierralearningacademy.com>
2025-08-11 17:29:48 +0530

    Add files via upload

---
+++ Example/EmbeddedC_Fundamentals.h
@@ -0,0 +1,9 @@
+#ifndef EMBEDDED_C_FUNDAMENTALS_H
+#define EMBEDDED_C_FUNDAMENTALS_H
+
+// Function prototypes
+void toggle_led(void);
+int calculate_sum(int a, int b);
+float compute_voltage(int adc_value);
+
+#endif // EMBEDDED_C_FUNDAMENTALS_H
