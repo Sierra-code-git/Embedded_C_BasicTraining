33bf9e33585f832cd5cfc0bccb11af785d5390e6
Sierra-code-git <Nireeksha@sierralearningacademy.com>
2025-09-03 09:28:35 +0530

    Delete SampleProblem_Session3.h

--- SampleProblem_Session3.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPLEPROBLEM_SESSION3_H
-#define SAMPLEPROBLEM_SESSION3_H
-
-#include <stdint.h>
-
-// Function to manipulate bits in a register
-uint8_t manipulate_register(uint8_t reg);
-
-#endif // SAMPLEPROBLEM_SESSION3_H
