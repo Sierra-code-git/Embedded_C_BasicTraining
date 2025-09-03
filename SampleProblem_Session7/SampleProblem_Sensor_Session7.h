b03cb1185c4f861929479decb25533f6c8cd995a
Sierra-code-git <Nireeksha@sierralearningacademy.com>
2025-09-03 09:25:27 +0530

    Delete SampleProblem_Session7 directory

--- SampleProblem_Session7/SampleProblem_Sensor_Session7.h
+++
@@ -1,8 +0,0 @@
-#ifndef SENSOR_H
-#define SENSOR_H
-
-void ADC_Init(void);
-uint16_t ADC_Read(uint8_t channel);
-float Read_Temperature(void);
-
-#endif
--- SampleProblem_Session7/SampleProblem_uart_Session7.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-void UART_Init(uint16_t baud);
-void UART_SendChar(char c);
-void UART_SendString(const char* str);
-
-#endif
